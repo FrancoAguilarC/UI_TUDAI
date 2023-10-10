bf3e1af1e37df465de67c01906ea2921ddb4bb51
Franco Damián <142952519+FrancoAguilarC@users.noreply.github.com>
2023-10-09 23:48:29 -0300

    Add files via upload

---
+++ TP2/js/loader.js
@@ -0,0 +1,30 @@
+let button = document.querySelectorAll('button.fake-loader');
+let loading = document.getElementById('loading');
+
+function actualizarPorcentaje() {
+    let porcentaje = 10; 
+    let porcentajeElement = document.getElementById('porcentaje');
+
+    let intervalo = setInterval(function() {
+        if (porcentaje < 100) {
+            porcentaje += 10; 
+            porcentajeElement.textContent = 'Cargando ' + porcentaje + '%';         } else {
+            clearInterval(intervalo); 
+        }
+        }, 500); 
+}
+
+
+function loadingCircle (e) {
+        e.preventDefault();
+        loading.classList.remove('no-visible');        
+        loading.classList.add('visible');
+        actualizarPorcentaje();
+        setTimeout(function() {
+            window.location.href = './index.html';
+        }, 5000);
+}
+
+button.forEach (b => {
+        b.addEventListener('click', loadingCircle);
+});
