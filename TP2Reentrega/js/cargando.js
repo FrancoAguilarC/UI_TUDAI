30acaa148f2fa934d5d82d7feaa21144986e4b29
Franco Damián <142952519+FrancoAguilarC@users.noreply.github.com>
2023-10-24 21:18:51 -0300

    Add files via upload

---
+++ TP2Reentrega/js/cargando.js
@@ -0,0 +1,30 @@
+let button = document.querySelectorAll('button.fake-loader');
+let loading = document.getElementById('loading');
+
+loadingCircle();
+
+function actualizarPorcentaje() {
+    let porcentaje = 10; 
+    let porcentajeElement = document.getElementById('porcentaje');
+
+    let intervalo = setInterval(function() {
+        if (porcentaje < 100) {
+            porcentaje += 10; 
+            porcentajeElement.textContent = 'Cargando ' + porcentaje + '%';} 
+            else {
+            clearInterval(intervalo); 
+        }
+        }, 500); 
+}
+
+
+function loadingCircle (e) {
+        e.preventDefault();
+        loading.classList.remove('no-visible');        
+        loading.classList.add('visible');
+        actualizarPorcentaje();
+        setTimeout(function() {
+            window.location.href = './home.html';
+        }, 5000);
+}
+
