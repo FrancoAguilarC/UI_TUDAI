d7b370582dfb636bec92dedfd9732a2f6684b7b4
Franco Damián <faguilarcarballo@alumnos.exa.unicen.edu.ar>
2023-11-28 22:58:14 -0300

    Carga TP4

--- TP4/js/spritesheet.js
+++
@@ -1,9 +0,0 @@
-const character = document.getElementById('character');
-
-character.addEventListener('mouseover', () => {
-    character.style.backgroundPosition = '0 -64px'; // Cambiar la posición en el spritesheet para la animación de hover
-});
-
-character.addEventListener('mouseout', () => {
-    character.style.backgroundPosition = '0 0'; // Restablecer la posición en el spritesheet cuando no está en hover
-});
